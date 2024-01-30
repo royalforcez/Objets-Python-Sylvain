643f4d627abeb94dbd6af0fb081fd7c46492af3a
xMokiii <morlioncolin@gmail.com>
2024-01-30 14:32:49 +0100

    Update flappy.py
    Mains Variables

--- Flappy-bird-python-master/Flappy-bird-python-master/flappy.py
+++ Flappy-bird-python-master/Flappy-bird-python-master/flappy.py
@@ -2,19 +2,19 @@ import pygame, random, time
 from pygame.locals import *
 
 #VARIABLES
-SCREEN_WIDHT = 400
-SCREEN_HEIGHT = 600
-SPEED = 20
-GRAVITY = 2.5
-GAME_SPEED = 15
+screen_width = 400
+screen_height = 600
+speed = 20
+gravity = 2.5
+game_speed = 15
 
-GROUND_WIDHT = 2 * SCREEN_WIDHT
-GROUND_HEIGHT= 100
+ground_width = 2 * screen_width
+ground_height= 100
 
-PIPE_WIDHT = 80
-PIPE_HEIGHT = 500
+pipe_width = 80
+pipe_height = 500
 
-PIPE_GAP = 150
+pipe_gap = 150
 
 wing = 'assets/audio/wing.wav'
 hit = 'assets/audio/hit.wav'
@@ -31,26 +31,26 @@ class Bird(pygame.sprite.Sprite):
                         pygame.image.load('assets/sprites/bluebird-midflap.png').convert_alpha(),
                         pygame.image.load('assets/sprites/bluebird-downflap.png').convert_alpha()]
 
-        self.speed = SPEED
+        self.speed = speed
 
         self.current_image = 0
         self.image = pygame.image.load('assets/sprites/bluebird-upflap.png').convert_alpha()
         self.mask = pygame.mask.from_surface(self.image)
 
         self.rect = self.image.get_rect()
-        self.rect[0] = SCREEN_WIDHT / 6
-        self.rect[1] = SCREEN_HEIGHT / 2
+        self.rect[0] = screen_width / 6
+        self.rect[1] = screen_height / 2
 
     def update(self):
         self.current_image = (self.current_image + 1) % 3
         self.image = self.images[self.current_image]
-        self.speed += GRAVITY
+        self.speed += gravity
 
         #UPDATE HEIGHT
         self.rect[1] += self.speed
 
     def bump(self):
-        self.speed = -SPEED
+        self.speed = -speed
 
     def begin(self):
         self.current_image = (self.current_image + 1) % 3
@@ -65,7 +65,7 @@ class Pipe(pygame.sprite.Sprite):
         pygame.sprite.Sprite.__init__(self)
 
         self. image = pygame.image.load('assets/sprites/pipe-green.png').convert_alpha()
-        self.image = pygame.transform.scale(self.image, (PIPE_WIDHT, PIPE_HEIGHT))
+        self.image = pygame.transform.scale(self.image, (pipe_width, pipe_height))
 
 
         self.rect = self.image.get_rect()
@@ -75,14 +75,14 @@ class Pipe(pygame.sprite.Sprite):
             self.image = pygame.transform.flip(self.image, False, True)
             self.rect[1] = - (self.rect[3] - ysize)
         else:
-            self.rect[1] = SCREEN_HEIGHT - ysize
+            self.rect[1] = screen_height - ysize
 
 
         self.mask = pygame.mask.from_surface(self.image)
 
 
     def update(self):
-        self.rect[0] -= GAME_SPEED
+        self.rect[0] -= game_speed
 
         
 
@@ -91,15 +91,15 @@ class Ground(pygame.sprite.Sprite):
     def __init__(self, xpos):
         pygame.sprite.Sprite.__init__(self)
         self.image = pygame.image.load('assets/sprites/base.png').convert_alpha()
-        self.image = pygame.transform.scale(self.image, (GROUND_WIDHT, GROUND_HEIGHT))
+        self.image = pygame.transform.scale(self.image, (ground_width, ground_height))
 
         self.mask = pygame.mask.from_surface(self.image)
 
         self.rect = self.image.get_rect()
         self.rect[0] = xpos
-        self.rect[1] = SCREEN_HEIGHT - GROUND_HEIGHT
+        self.rect[1] = screen_height - ground_height
     def update(self):
-        self.rect[0] -= GAME_SPEED
+        self.rect[0] -= game_speed
 
 def is_off_screen(sprite):
     return sprite.rect[0] < -(sprite.rect[2])
@@ -107,16 +107,16 @@ def is_off_screen(sprite):
 def get_random_pipes(xpos):
     size = random.randint(100, 300)
     pipe = Pipe(False, xpos, size)
-    pipe_inverted = Pipe(True, xpos, SCREEN_HEIGHT - size - PIPE_GAP)
+    pipe_inverted = Pipe(True, xpos, screen_height - size - pipe_gap)
     return pipe, pipe_inverted
 
 
 pygame.init()
-screen = pygame.display.set_mode((SCREEN_WIDHT, SCREEN_HEIGHT))
+screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption('Flappy Bird')
 
 BACKGROUND = pygame.image.load('assets/sprites/background-day.png')
-BACKGROUND = pygame.transform.scale(BACKGROUND, (SCREEN_WIDHT, SCREEN_HEIGHT))
+BACKGROUND = pygame.transform.scale(BACKGROUND, (screen_width, screen_height))
 BEGIN_IMAGE = pygame.image.load('assets/sprites/message.png').convert_alpha()
 
 bird_group = pygame.sprite.Group()
@@ -126,12 +126,12 @@ bird_group.add(bird)
 ground_group = pygame.sprite.Group()
 
 for i in range (2):
-    ground = Ground(GROUND_WIDHT * i)
+    ground = Ground(ground_width * i)
     ground_group.add(ground)
 
 pipe_group = pygame.sprite.Group()
 for i in range (2):
-    pipes = get_random_pipes(SCREEN_WIDHT * i + 800)
+    pipes = get_random_pipes(screen_width * i + 800)
     pipe_group.add(pipes[0])
     pipe_group.add(pipes[1])
 
@@ -161,7 +161,7 @@ while begin:
     if is_off_screen(ground_group.sprites()[0]):
         ground_group.remove(ground_group.sprites()[0])
 
-        new_ground = Ground(GROUND_WIDHT - 20)
+        new_ground = Ground(ground_width - 20)
         ground_group.add(new_ground)
 
     bird.begin()
@@ -191,14 +191,14 @@ while True:
     if is_off_screen(ground_group.sprites()[0]):
         ground_group.remove(ground_group.sprites()[0])
 
-        new_ground = Ground(GROUND_WIDHT - 20)
+        new_ground = Ground(ground_width - 20)
         ground_group.add(new_ground)
 
     if is_off_screen(pipe_group.sprites()[0]):
         pipe_group.remove(pipe_group.sprites()[0])
         pipe_group.remove(pipe_group.sprites()[0])
 
-        pipes = get_random_pipes(SCREEN_WIDHT * 2)
+        pipes = get_random_pipes(screen_width * 2)
 
         pipe_group.add(pipes[0])
         pipe_group.add(pipes[1])
