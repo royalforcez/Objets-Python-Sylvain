36eeacd164672aa39bd2974713d66ba7666f6f20
xMokiii <morlioncolin@gmail.com>
2024-02-13 14:33:08 +0100

    Etape 1 : Classe carré fonctionelle

---
+++ heritage.py
@@ -0,0 +1,24 @@
+#heritage ASCII
+
+class Carre:
+    def __init__(self, x, y, cote):
+        self.x = x
+        self.y = y
+        self.cote = cote
+
+    def dessiner(self, ecran):
+        for i in range(self.y, self.y + self.cote):
+            for j in range(self.x, self.x + self.cote):
+                ecran[i][j] = '*'
+
+#variables
+LARGEUR = 50
+HAUTEUR = 20
+
+#code
+ecran = [ ['.' for _ in range(LARGEUR)] for _ in range(HAUTEUR) ]
+carre = Carre(x=1, y=2, cote=3)
+carre.dessiner(ecran)
+for ligne in ecran:
+    print(' '.join(ligne))
+
