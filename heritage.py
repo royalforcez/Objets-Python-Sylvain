85f01730c9746eabfd973b267d504f12d4d642b5
xMokiii <morlioncolin@gmail.com>
2024-02-13 14:54:22 +0100

    Classe Forme fonctionnelle

--- heritage.py
+++ heritage.py
@@ -1,36 +1,35 @@
 #heritage ASCII
 
 class Forme:
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
+    def __init__(self, x: int, y: int):
+        self._x = x
+        self._y = y
 
 
-
-
-
-
-class Carre:
-    def __init__(self, x, y, cote):
-        self.x = x
-        self.y = y
-        self.cote = cote
+class Carre(Forme):
+    def __init__(self, x: int, y: int, cote: int):
+        super().__init__(x, y)
+        self._cote = cote
 
     def dessiner(self, ecran):
-        for i in range(self.y, self.y + self.cote):
-            for j in range(self.x, self.x + self.cote):
+        for i in range(self._y, self._y + self._cote):
+            for j in range(self._x, self._x + self._cote):
                 ecran[i][j] = '*'
 
 
-
-#variables
+# Variables
 LARGEUR = 50
 HAUTEUR = 20
 
-#code
-ecran = [ ['.' for _ in range(LARGEUR)] for _ in range(HAUTEUR) ]
-carre = Carre(x=1, y=2, cote=3)
-carre.dessiner(ecran)
+# Code
+ecran = [['.' for _ in range(LARGEUR)] for _ in range(HAUTEUR)]
+formes = [
+    Carre(x=1, y=2, cote=3),
+    Carre(x=5, y=3, cote=4)
+]
+
+for forme in formes:
+    forme.dessiner(ecran)
+
 for ligne in ecran:
     print(' '.join(ligne))
-
